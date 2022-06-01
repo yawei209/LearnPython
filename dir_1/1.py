73ebec266f8ff291fc3ee28aa791860a6048106b
yawei_zhang <yawei_zhang@bizconf.cn>
2022-06-01 20:09:36 +0800

    first save

---
+++ dir_1/1.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Time : 2022/6/1 19:34
+# @Author : zhangyawei
+# @Email : zhangyawei209@163.com
+# @File : 1.py.py
+# @Software: PyCharm
+
+print('Hello World')
---
+++ dir_1/2.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# @Time : 2022/6/1 19:34
+# @Author : zhangyawei
+# @Email : zhangyawei209@163.com
+# @File : 1.py.py
+
+
+# print('The quick borwn fox', 'jumps over', 'the lazy dog')
+#
+# print('100 + 200 =', 100+200)
+
+name = input('Please input your name:')
+
+print('My name is ', name)
+
+
