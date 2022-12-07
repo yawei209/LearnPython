3b7babe8c3913c7c04fb653bbca2e80e7a7fe34a
zhangyawei <zhang.yawei@geelytalent.com.cn>
2022-12-07 21:18:00 +0800

    pytest mark.skip、mark.skipif等

--- 00pytest/testcase/test_py.py
+++ 00pytest/testcase/test_py.py
@@ -23,21 +23,41 @@ class Testcase:
     # def teardown(self):
     #     print('这是用例执行之后的teardown')
 
+##############pytest.mark.run(order)的用法############
+
     @pytest.mark.run(order=2)
     def test_01(self):
-        # print('这是第一条测试用例')
-        # assert 1 == 12
+        print('这是第一条测试用例')
         pass
 
     @pytest.mark.run(order=1)
     def test_02(self):
-        # print('这是第二条测试用例')
-        # assert 1 == '1'
+        print('这是第二条测试用例')
+        pass
+
+#####################################################
+    @pytest.mark.pub
+    def test_03(self):
+        print('这是第三条测试用例')
+        pass
+    @pytest.mark.pub
+    def test_04(self):
+        print('这是第四条测试用例')
+        pass
+
+    @pytest.mark.skip(reason = "强制跳过test_05了")
+    def test_05(self):
+        print('这是第5条测试用例')
+        pass
+
+    @pytest.mark.skipif(True, reason="有条件跳过test_06了")
+    def test_06(self):
+        print('这是第6条测试用例')
         pass
 
 #执行用例：1.用命令运行pytest。 2.main方法运行
 
 #pytest.main() 中增加参数 -s、-v 显示详细用例
 if __name__ == '__main__':
-    pytest.main(['-s','-v','test_py.py'])
+    pytest.main(['-s','-v','-m' 'pub','test_py.py'])
 
