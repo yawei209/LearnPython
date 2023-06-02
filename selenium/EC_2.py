c54fcb85f1e680d69c4e0e019d6a826efd586f50
zhangyawei <zhang.yawei@geelytalent.com.cn>
2023-06-02 16:05:52 +0800

    Revert "修改EC_2文档"
    This reverts commit 0f53e41065dd27caf408d4564cece1c71101cf11.

--- selenium/EC_2.py
+++ selenium/EC_2.py
@@ -6,30 +6,10 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from time import sleep
-import pytest
 
-# driver = webdriver.Chrome()
-# driver.get('https://www.baidu.com')
-# WebDriverWait(driver, 10).until(EC.title_is("百度一下，你就知道"))
-#
-# element = WebDriverWait(driver, 10).until(EC.visibility_of(driver.find_element(By.ID, 'kw')))
-# element.send_keys("测试测试！")
+driver = webdriver.Chrome()
+driver.get('https://www.baidu.com')
+WebDriverWait(driver, 10).until(EC.title_is("百度一下，你就知道"))
 
-class TestDemo():
-    def setup(self):
-        self.driver = webdriver.Chrome()
-
-    def teardown(self):
-        sleep(2)
-        self.driver.quit()
-
-    def test_searchinputbox_is_visibility(self):
-        self.driver.get('https://www.baidu.com')
-        element = self.driver.find_element(By.ID, 'kw')
-        result = WebDriverWait(self.driver, 10).until(EC.visibility_of(element)) #如果存在，则返回这个元素。
-        result.send_keys('测试测试')
-        
-
-if __name__ == '__main__':
-    pytest.main(['-s', '-v', 'EC_2.PY'])
+element = WebDriverWait(driver, 10).until(EC.visibility_of(driver.find_element((By.ID, 'kw'))))
+element.send_keys("测试测试！")
